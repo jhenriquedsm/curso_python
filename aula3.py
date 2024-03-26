03bbcc9b10450d5c7aaf690d6ef3410ab181fa8e
jhenriquedsm <jhsilvamata@gmail.com>
2024-03-26 13:41:31 -0300

    aula 3

---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de Aspas
+
+"""
+
+# Aspas Simples
+print('Jose Henrique')
+print('Jose "Henrique"')
+
+# Aspas Duplas
+print("Jose Henrique")
+print("Jose 'Henrique'")
+
+# Escape
+print("Jose \"Henrique\"")
+
+# r (expressões regulares)
+print(r"Jose \"Henrique\"") 
